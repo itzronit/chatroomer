081225b9094068ff7a5f1716caaa8683c6f2cc46
s1gaming <banerjeesayantan1609@gmail.com>
2023-07-11 18:29:01 +0530

    Added new endpoint to create a user.

---
+++ app/api/createUser/route.ts
@@ -0,0 +1,36 @@
+import { connectDB, disconnectDB } from "../../../database";
+import User from "../../../schemas/user";
+import { NextRequest, NextResponse } from "next/server";
+
+connectDB();
+
+export async function POST(req: Request) {
+  //@ts-ignore
+  const { email, username, fullName, password } = await req.json();
+  //@ts-check
+  const missingFields = [];
+
+  if (!email) missingFields.push('email');
+  if (!username) missingFields.push('username');
+  if (!fullName) missingFields.push('fullName');
+  if (!password) missingFields.push('password');
+
+  if (missingFields.length > 0) {
+    disconnectDB();
+    return NextResponse.json({ message: `Missing fields: ${missingFields.join(', ')}`, body: req.json() }, { status: 400 })
+  }
+
+  const user = new User({
+    email,
+    username,
+    fullName,
+    password
+  });
+
+  await user.save();
+
+  disconnectDB();
+
+  return NextResponse.json({ message: 'User created successfully' });
+
+}

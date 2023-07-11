39fff7a7d66a852d33091a22bf3c9cf7b262d493
s1gaming <banerjeesayantan1609@gmail.com>
2023-07-11 18:30:14 +0530

    Using Mongoose Wrapper to connect to database

--- database.ts
+++ database.ts
@@ -1,18 +1,17 @@
-import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
-const uri = `${process.env.MONGO_FULL_URL}`;
-export const client = new MongoClient(uri);
+const uri = `${process.env.MONGO_FULL_URL}/${process.env.MONGO_DATABASE_NAME}`;
 
 export function connectDB() {
-    client.connect().then(() => {
-        console.log("Connected to MongoDB");
-    })
+    mongoose.connect(uri).then(() => {
+        console.log("Connected to MongoDB(Mongoose Wrapper)");
+    });
 }
 
 export function disconnectDB() {
-    client.close().then(() => {
-        console.log("Disconnected from MongoDB");
-    })
+    mongoose.disconnect().then(() => {
+        console.log("Disconnected from MongoDB(Mongoose Wrapper)");
+    });
 }

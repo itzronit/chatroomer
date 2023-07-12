5fffb8bfe1ad5fff6ccc7d13579ce3148da0dc3e
s1gaming <banerjeesayantan1609@gmail.com>
2023-07-12 23:09:41 +0530

    Removed unnecessary package and added comments

--- database.ts
+++ database.ts
@@ -1,13 +1,24 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
-dotenv.config();
 
 const uri = `${process.env.MONGO_FULL_URL}/${process.env.MONGO_DATABASE_NAME}`;
 
 export function connectDB() {
-    mongoose.connect(uri).then(() => {
-        console.log("Connected to MongoDB(Mongoose Wrapper)");
-    });
+
+    /* 
+    Mongoose readyState meanings:
+
+    0: disconnected
+    1: connected
+    2: connecting
+    3: disconnecting
+
+    */
+
+    if (mongoose.connection.readyState === 0) {
+        mongoose.connect(uri).then(() => {
+            console.log("Connected to MongoDB(Mongoose Wrapper)");
+        });
+    }
 }
 
 export function disconnectDB() {

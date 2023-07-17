da8fd39f4fa25f1c483059951af58d5b995552a0
s1gaming <banerjeesayantan1609@gmail.com>
2023-07-18 01:24:27 +0530

    Added new Global Variables for User Session

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,8 @@
+//@ts-nocheck
+import type { LayoutServerLoad } from "./$types";
+
+export const load: LayoutServerLoad = async (event) => {
+  return {
+    session: await event.locals.getSession(),
+  };
+};

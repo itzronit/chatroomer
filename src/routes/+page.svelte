0b387899faa7b191f01f1a4cd351c89a6b933755
s1gaming <banerjeesayantan1609@gmail.com>
2023-07-17 13:49:50 +0530

    Added Basic Implementation of Global Variables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Hello World</h1>
+<h1>Chat Roomer</h1>
+<p>Welcome to basic chat roomer project home page</p>
+
+<style lang="scss">
+    @import '/src/styles/variable';
+
+    h1 {
+        color: $color;
+    }
+</style>
